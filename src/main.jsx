b95ce3587bdf7608d658205379ad6c178704ca4d
Arman Mir <mirarman8583@gmail.com>
2025-10-31 15:10:46 +0600

    Implement routing with react-router and create Home, AllProducts, and RootLayout components

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from './Layout/RootLayout.jsx';
+import Home from './Components/Home/Home.jsx';
+import AllProducts from './Components/AllProducts/AllProducts.jsx';
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path: '/allProducts',
+        Component: AllProducts
+      }
+    ]
+  },
+]);
+
+
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
